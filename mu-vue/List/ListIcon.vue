9f6909cfc2fd8a69d8d5beef6b260f625721c4e1
ly Spice <ly Spice>
2017-03-05 20:42:21 +0800

    []

--- mu-vue/List/ListIcon.vue
+++ mu-vue/List/ListIcon.vue
@@ -1,3 +1,16 @@
 <script>
-    
+/**
+ *  time 2017/03/03
+ *  name Spicely
+ *  email Spicely@outlook.com
+ */
+    export default {
+        name: "list-icon",
+        props: {
+            num: {
+
+            }
+        }
+    }
+
 </script>
