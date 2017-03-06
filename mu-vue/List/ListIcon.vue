5965d0ccc9683cc54dd937c104025945bb00905e
Spicely <Spicely@outlook.com>
2017-03-06 17:59:10 +0800

    []

--- mu-vue/List/ListIcon.vue
+++ mu-vue/List/ListIcon.vue
@@ -1,16 +1,94 @@
+<style>
+    .list-icon-pack {
+        padding: 5px 0;
+    }
+    
+    ul.list-icon-line {
+        display: flex;
+        padding: 10px 0;
+    }
+    
+    li.list-icon-list {
+        flex: 1;
+    }
+    li img.list-icon-img{
+        display: block;
+        margin: auto;
+    }
+    li div.list-icon-lable{
+        text-align: center;
+    }
+</style>
 <script>
-/**
- *  time 2017/03/03
- *  name Spicely
- *  email Spicely@outlook.com
- */
+    import music from "../../../assets/icon/Cloud_Music.png";
+    /**
+     *  time 2017/03/03
+     *  name Spicely
+     *  email Spicely@outlook.com
+     */
     export default {
-        name: "list-icon",
+        name: "List-Icon",
+        render: function(createElement) {
+            let iconList = [];
+            if (this.num) {
+                let line = Math.ceil(this.params.length / this.num);
+                for (var i = 0; i < line; i++) {
+                    iconList.push(createElement("ul", {
+                        class: {
+                            "list-icon-line": true
+                        }
+                    }, this.params.map((item, key) => {
+                        if (key >= i * this.num && key < (i * this.num + this.num)) {
+                            return createElement("li", {
+                                class: {
+                                    "list-icon-list": true
+                                }
+                            }, [createElement("img", {
+                                class: {
+                                    "list-icon-img": true
+                                },
+                                attrs: {
+                                    src: item.src
+                                }
+                            }), createElement("div", {
+                                class: {
+                                    "list-icon-lable": true
+                                }
+                            })]);
+                        }
+                    })));
+                }
+            } else {
+                iconList.push(createElement("ul", {
+                    class: {
+                        "list-icon-line": true
+                    }
+                }, this.params.map((item, key) => {
+                    return createElement("li", {
+                        class: {
+                            "list-icon-list": true
+                        }
+                    });
+                })));
+            }
+            return createElement("div", {
+                class: {
+                    "list-icon-pack": true
+                }
+            }, iconList);
+        },
         props: {
             num: {
-
+                type: Number,
+                default: 0
+            },
+            params: {
+                type: Array,
+                default: () => []
             }
+        },
+        methods: {
+
         }
     }
-
 </script>
