64d25db132d276e87d45cc668c48f5b71e960227
Spicely <Spicely@outlook.com>
2017-03-21 18:02:12 +0800

    []

--- mu-vue/Scroll/ScrollPage.vue
+++ mu-vue/Scroll/ScrollPage.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="mu-screen-page-pack">
-        <div class="mu-screen-page-hold" v-scroll="onScroll">
+    <div class="mu-scroll-page-pack">
+        <div class="mu-scroll-page-hold" v-scroll="onScroll">
             <slot></slt>
         </div>
     </div>
 </template>
 
 <style lang="less">
-    .mu-screen-page-pack {
+    .mu-scroll-page-pack {
         overflow: hidden;
-        .mu-screen-page-hold {}
+        .mu-scroll-page-hold {}
     }
 </style>
 <script>
@@ -21,41 +21,14 @@
      *      提供用于滚动的组件
      *      用于PC和Mobile中都能使用的组件
      */
+    import "./scroll";
     export default {
-        name: "screen-page",
+        name: "scroll-page",
         methods: {
             onScroll: function() {
-                console.log(1)
+                console.log(2)
             }
         },
-        directive: {
-            scroll: {
-                bind: function(el, binding, vnode) {
-                    if (!binding.value || typeof binding.value !== 'function') {
-                        throw new Error('The scroll listener is not a function');
-                    }
-                },
-                inserted: function(el, binding) {
-                    //To do, check whether el is scrollable and give warn message if it's not'
-                },
-                update: function(el, binding) {
-                    if (binding.value === binding.oldValue) {
-                        return;
-                    }
-                    if (!binding.value || typeof binding.value !== 'function') {
-                        throw new Error('The scroll listener is not a function');
-                    }
-                },
-                unbind: function(el, binding) {
-                    if (binding.value && typeof binding.value === 'function') {
-                        if (el.removeEventListener) {
-                            el.removeEventListener('scroll', binding.value);
-                        } else {
-                            el.detachEvent('onscroll', binding.value);
-                        }
-                    }
-                }
-            }
-        }
+
     }
 </script>
---
+++ mu-vue/Scroll/scroll.js
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+import Vue from 'vue';
+import lang from "../../mu/base/lang";
+(function() {
+    if (!Vue) {
+        throw new Error("No load Vue.js file");
+    }
+    let handleListeners = function(el, current, old) {
+        if (el == document.body) {
+            document.onscroll = function(e) {
+                current && current(e, {
+                    scrollTop: document.body.scrollTop,
+                    scrollLeft: document.body.scrollLeft
+                });
+            }
+        } else if (el.addEventListener) {
+            // 先清除滚动事件
+            old && el.removeEventListener('scroll', old);
+            el.addEventListener('scroll', function (e) {
+                current && current(e, {
+                    scrollTop: el.scrollTop,
+                    scrollLeft: el.scrollLeft
+                });
+            });
+        } else {
+            old && el.detachEvent('onscroll', old);
+            el.attach('onscroll', function(e) {
+                e = e || window.event;
+                current && current(e, {
+                    scrollTop: el.scrollTop,
+                    scrollLeft: el.scrollLeft
+                });
+            });
+        }
+    }
+    // 注册一个全局自定义指令 v-scorll
+    Vue.directive('scroll', {
+        // 当绑定元素插入到 DOM 中。
+        bind: function(el, binding, vnode) {
+            handleListeners(el, binding.value, binding.oldValue);
+        },
+        inserted: function(el, binding) {
+            //To do, check whether el is scrollable and give warn message if it's not'
+        },
+        update: function(el, binding) {
+            if (binding.value === binding.oldValue) {
+                return;
+            }
+            if (!binding.value || !lang.isFunction(binding.value)) {
+                throw new Error('The scroll listener is not a function');
+            }
+            handleListeners(el, binding.value, binding.oldValue);
+        },
+        unbind: function(el, binding) {
+            if (binding.value && lang.isFunction(binding.value)) {
+                if (el.removeEventListener) {
+                    el.removeEventListener('scroll', binding.value);
+                } else {
+                    el.detachEvent('onscroll', binding.value);
+                }
+            }
+        }
+    });
+})()
