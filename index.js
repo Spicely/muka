7e1717a0255228aefcfaf88340e2df5fb156d248
Spicely <Spicely@outlook.com>
2017-07-06 19:04:50 +0800

    add mouseDrag touch

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import xhr from './lib/xhr'
 import json from './lib/json'
 import config from './lib/config'
+import browser from './lib/browser'
 import lang from './lib/base/lang'
-import browser from './lib/base/browser'
+import domStyle from './lib/dom/style'
+import mouseDrag from './lib/event/mouseDrag'
 const muka = config.getObject()
 export {
     muka as
     default,
     xhr,
     json,
+    browser,
     config,
     lang,
-    browser
+    domStyle,
+    mouseDrag
 }
