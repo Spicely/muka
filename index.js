ad86beafa14919e14071c32272f69434f65d0e29
Spicely <Spicely@outlook.com>
2017-07-10 18:59:56 +0800

    []

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import xhr from './lib/xhr'
 import domStyle from './lib/domStyle'
 import drag from './lib/drag'
+import lang from './lib/lang'
 export {
     xhr,
     drag,
-    domStyle
+    domStyle,
+    lang
 }
