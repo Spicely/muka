336535a62a83080867c69fc4e343513bb2d0d2d9
ly Spice <ly Spice>
2017-07-21 22:34:10 +0800

    []

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-import xhr from './lib/xhr'
-import domStyle from './lib/domStyle'
-import drag from './lib/drag'
-import lang from './lib/lang'
-export {
-    xhr,
-    drag,
-    domStyle,
-    lang
-}
+import muka from './lib'
+module.exports = muka
