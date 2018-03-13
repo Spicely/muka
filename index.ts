37a95fb105aeeaffd237c85a543884bfb01a16d9
Spicely <Spicely@outlook.com>
2018-03-14 00:29:22 +0800

    增加json配置 获得json对象

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export { default as date } from './lib/date'
 export { default as lang } from './lib/lang'
+export { default as json } from './lib/json'
 export { default as type } from './lib/type'
 export { default as verify } from './lib/verify'
